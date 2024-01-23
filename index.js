8bead9ea245116602798f8e4f5b4375cae6f2515
Anas0055 <manaskhan969@gmail.com>
2024-01-23 02:15:22 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Number Eight: Write addition, subtraction, multiplication,
+//  and division operations that each result in the number 8.
+//   Be sure to enclose your operations in print statements to
+//    see the results. 
+//add
+var addition = 5 + 3;
+console.log("addition:", addition);
+//sub
+var sub = 10 - 2;
+console.log("sub:", sub);
+//mul
+var mul = 8 * 1;
+console.log("mul:", mul);
+//div
+var div = 16 / 2;
+console.log("div:", div);
---
+++ index.ts
@@ -0,0 +1,16 @@
+// Number Eight: Write addition, subtraction, multiplication,
+//  and division operations that each result in the number 8.
+//   Be sure to enclose your operations in print statements to
+//    see the results. 
+      //add
+ let addition:number=5+3;
+ console.log ("addition:",addition);
+      //sub
+  let sub:number=10-2;
+  console.log("sub:",sub);
+      //mul
+  let mul:number=8*1;
+  console.log ("mul:",mul);
+      //div
+  let div: number=16/2;
+  console.log ("div:",div);                 
